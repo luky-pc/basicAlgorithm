1f374a7f74fc105bd3273446432ecb86f54b0a3c
luky <looker_pc@163.com>
2019-01-05 15:52:37 +0800

    非递归处理合并排序

---
+++ mergeSort/mergeSort.js
@@ -0,0 +1,40 @@
+/**
+ * @author: create by eleven
+ * @version: v1.0
+ * @description:
+ * @date:2019/1/5
+ **/
+/**
+ * 使用非递归方式实现合并排序
+ * @param inputArr
+ * @returns {*}
+ */
+function mergeSort(inputArr=[]){
+    let temArr=inputArr.map((item)=>{return [item]}),excessiveArr,childArr=[],index,
+    join=(firstArr=[],secondArr=[])=>{
+        let result=[];
+        while(firstArr.length||secondArr.length){
+            if(firstArr[0]<secondArr[0]||!secondArr[0]){
+                result.push(firstArr[0]);
+                firstArr.shift();
+            }else{
+                result.push(secondArr[0]);
+                secondArr.shift();
+            }
+        }
+        return result;
+    };
+    while(temArr.length>1){
+        excessiveArr=[...temArr];
+        temArr=[];
+        index=0;
+        while(index<excessiveArr.length){
+            childArr=join(excessiveArr[index],excessiveArr[index+1]);
+            temArr.push(childArr);
+            index+=2;
+        }
+    }
+    return temArr[0];
+}
+let a=mergeSort([103,24,25,12,305,47,58,90,77]);
+console.log(a);
